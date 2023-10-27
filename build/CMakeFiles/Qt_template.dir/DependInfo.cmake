fe69585634a549a4db6821d15d1bd6bdc82fc4fe
SHKim-HYU <tjsghd101@naver.com>
2023-10-27 16:33:17 +0900

    qt template

---
+++ build/CMakeFiles/Qt_template.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/robot/robot_ws/QT_template/build/Qt_template_autogen/mocs_compilation.cpp" "/home/robot/robot_ws/QT_template/build/CMakeFiles/Qt_template.dir/Qt_template_autogen/mocs_compilation.cpp.o"
+  "/home/robot/robot_ws/QT_template/src/main.cpp" "/home/robot/robot_ws/QT_template/build/CMakeFiles/Qt_template.dir/src/main.cpp.o"
+  "/home/robot/robot_ws/QT_template/src/mainwindow.cpp" "/home/robot/robot_ws/QT_template/build/CMakeFiles/Qt_template.dir/src/mainwindow.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NETWORK_LIB"
+  "QT_QMLMODELS_LIB"
+  "QT_QML_LIB"
+  "QT_QUICK_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "Qt_template_autogen/include"
+  "../include/QtWidgets"
+  "../include/DarkStyleWindow"
+  "../include/DarkStyleWindow/framelesswindow"
+  "/opt/Qt5.14.2/5.14.2/gcc_64/include"
+  "/opt/Qt5.14.2/5.14.2/gcc_64/include/QtWidgets"
+  "/opt/Qt5.14.2/5.14.2/gcc_64/include/QtGui"
+  "/opt/Qt5.14.2/5.14.2/gcc_64/include/QtCore"
+  "/opt/Qt5.14.2/5.14.2/gcc_64/./mkspecs/linux-g++"
+  "/opt/Qt5.14.2/5.14.2/gcc_64/include/QtNetwork"
+  "/opt/Qt5.14.2/5.14.2/gcc_64/include/QtQuick"
+  "/opt/Qt5.14.2/5.14.2/gcc_64/include/QtQmlModels"
+  "/opt/Qt5.14.2/5.14.2/gcc_64/include/QtQml"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/robot/robot_ws/QT_template/build/include/QtWidgets/CMakeFiles/QtWidgets.dir/DependInfo.cmake"
+  "/home/robot/robot_ws/QT_template/build/include/DarkStyleWindow/CMakeFiles/DarkStyleWindow.dir/DependInfo.cmake"
+  "/home/robot/robot_ws/QT_template/build/include/DarkStyleWindow/framelesswindow/CMakeFiles/framelesswindow.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Qt_template.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Qt_template_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/Qt_template_autogen.dir/ParseCache.txt"
+  "Qt_template_autogen"
+  "CMakeFiles/Qt_template.dir/Qt_template_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/Qt_template.dir/src/main.cpp.o"
+  "CMakeFiles/Qt_template.dir/src/mainwindow.cpp.o"
+  "Qt_template"
+  "Qt_template.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Qt_template.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
